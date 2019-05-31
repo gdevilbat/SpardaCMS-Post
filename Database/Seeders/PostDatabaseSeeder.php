dbad6e3d4b2097a906745e52a72c81521889a580
unknown <maldinuribrahim@gmail.com>
2019-05-31 14:34:42 +0700

    Initial Commit

---
+++ Database/Seeders/PostDatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Gdevilbat\SpardaCMS\Modules\Post\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class PostDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+
+        // $this->call("OthersTableSeeder");
+    }
+}
