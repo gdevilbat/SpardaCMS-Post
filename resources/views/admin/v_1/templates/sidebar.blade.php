2f8515bbd6a8a464f58b817be0df1f5a56be19fe
unknown <maldinuribrahim@gmail.com>
2019-12-14 15:30:21 +0700

    Fix Sidebar

--- resources/views/admin/v_1/templates/sidebar.blade.php
+++ resources/views/admin/v_1/templates/sidebar.blade.php
@@ -7,12 +7,9 @@
          </a>
         <div class="m-menu__submenu "><span class="m-menu__arrow"></span>
             <ul class="m-menu__subnav">
+                <li class="m-menu__item  m-menu__item--parent" aria-haspopup="true"><span class="m-menu__link"><span class="m-menu__link-text">{{ trans_choice('post::messages.post', 2) }}</span></span></li>
                 <li class="m-menu__item  {{strstr(Route::current()->getName(), 'cms.post-data') ? 'm-menu__item--active' : ''}}" aria-haspopup="true"><a href="{{route('cms.post-data.master')}}" class="m-menu__link "><i class="m-menu__link-bullet m-menu__link-bullet--dot"><span></span></i><span class="m-menu__link-text">All {{ trans_choice('post::messages.post', 1) }}</span></a></li>
-            </ul>
-            <ul class="m-menu__subnav">
                 <li class="m-menu__item  {{strstr(Route::current()->getName(), 'cms.post-category') ? 'm-menu__item--active' : ''}}" aria-haspopup="true"><a href="{{route('cms.post-category.master')}}" class="m-menu__link "><i class="m-menu__link-bullet m-menu__link-bullet--dot"><span></span></i><span class="m-menu__link-text">{{ trans_choice('post::messages.post_category', 2) }}</span></a></li>
-            </ul>
-            <ul class="m-menu__subnav">
                 <li class="m-menu__item  {{strstr(Route::current()->getName(), 'cms.post-tag') ? 'm-menu__item--active' : ''}}" aria-haspopup="true"><a href="{{route('cms.post-tag.master')}}" class="m-menu__link "><i class="m-menu__link-bullet m-menu__link-bullet--dot"><span></span></i><span class="m-menu__link-text">{{ trans_choice('post::messages.post_tag', 2) }}</span></a></li>
             </ul>
         </div>
