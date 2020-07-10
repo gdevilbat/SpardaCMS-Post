7eca81fa9fab0188e4eb69b74046928388b1a36b
maldinuribrahim <maldinuribrahim@gmail.com>
2020-07-11 05:41:00 +0700

    Fix User Role

--- resources/views/admin/v_1/content/Post/master.blade.php
+++ resources/views/admin/v_1/content/Post/master.blade.php
@@ -63,7 +63,7 @@
                     @endif
                 </div>
 
-                @can('create-post')
+                @if(Auth::user()->can('create-post'))
                     <div class="row mb-4">
                         <div class="col-md-5">
                             <a href="{{route('cms.post-data.create')}}" class="btn btn-brand m-btn m-btn--custom m-btn--icon m-btn--pill m-btn--air">
@@ -74,7 +74,18 @@
                             </a>
                         </div>
                     </div>
-                @endcan
+                @else
+                    <div class="row mb-4">
+                        <div class="col-md-5">
+                            <a href="javascript:void(0)" class="btn btn-brand m-btn m-btn--custom m-btn--icon m-btn--pill m-btn--air"  data-toggle="m-popover" title="" data-content="You're not Allowed To Take This Action. Pleas Ask Admin !!!" data-original-title="Forbidden Action">
+                                <span>
+                                    <i class="la la-plus"></i>
+                                    <span>Add New {{trans_choice('post::messages.post', 1)}}</span>
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                @endif
 
                 <!--begin: Datatable -->
                 <table class="table table-striped display responsive nowrap data-table-ajax" id="data-post" data-ajax="{{route('cms.post-data.service-master')}}" width="100%">
