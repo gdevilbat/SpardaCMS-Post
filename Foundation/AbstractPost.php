f1b276b9bc8adc4aa9031eee0cf3c600efd998e0
maldinuribrahim <maldinuribrahim@gmail.com>
2021-07-03 20:43:11 +0700

    Post CMS search By Category

--- Foundation/AbstractPost.php
+++ Foundation/AbstractPost.php
@@ -69,6 +69,9 @@ abstract class AbstractPost extends CoreController implements InterfacePost
                                 ->orWhereHas('taxonomies.term', function($query) use ($searchValue){
                                     $query->where(DB::raw("CONCAT(name,'-',slug)"), 'like', '%'.$searchValue.'%');
                                 })
+                                ->orWhereHas('taxonomies.allTaxonomyParents.term', function($query) use ($searchValue){
+                                    $query->where(DB::raw("CONCAT(name,'-',slug)"), 'like', '%'.$searchValue.'%');
+                                })
                                 ->orWhereHas('author', function($query) use ($searchValue){
                                     $query->where(DB::raw("CONCAT(name)"), 'like', '%'.$searchValue.'%');
                                 });
