6140cee0f123f205417d4e3279daff62f36ceab3
maldinuribrahim <maldinuribrahim@gmail.com>
2021-09-11 21:31:45 +0700

    Fix Post Link Homepage

--- Entities/Post.php
+++ Entities/Post.php
@@ -95,6 +95,16 @@ class Post extends Model
 
     final function getPostURLAttribute()
     {
+        if($this->post_slug == 'homepage')
+        {
+            return url('/');
+        }
+
+        if($this->post_type == 'post')
+        {
+            return url($this->created_at->format('Y').'/'.$this->created_at->format('m').'/'.$this->post_slug.'.html');
+        }
+
         if($this->post_type == 'page')
         {
             return url($this->post_slug);
